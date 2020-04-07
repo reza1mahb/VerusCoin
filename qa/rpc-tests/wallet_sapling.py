dc47399ce60f27fcb84d2c3621313296ba56438c
Alfredo Garcia <oxarbitrage@gmail.com>
2020-04-07 10:39:33 -0300

    add expected address check to tests

--- qa/rpc-tests/wallet_sapling.py
+++ qa/rpc-tests/wallet_sapling.py
@@ -200,10 +200,12 @@ class WalletSaplingTest(BitcoinTestFramework):
         extfvk0 = self.nodes[0].z_exportviewingkey(saplingAddr0)
         saplingAddrInfo0 = self.nodes[3].z_importviewingkey(extfvk0, "yes")
         assert_equal(saplingAddrInfo0["type"], "sapling")
+        assert_equal(saplingAddrInfo0["address"], saplingAddr0)
         assert_equal(self.nodes[3].z_getbalance(saplingAddrInfo0["address"]), Decimal('10'))
         extfvk1 = self.nodes[1].z_exportviewingkey(saplingAddr1)
         saplingAddrInfo1 = self.nodes[3].z_importviewingkey(extfvk1, "yes")
-        assert_equal(saplingAddrInfo0["type"], "sapling")
+        assert_equal(saplingAddrInfo1["type"], "sapling")
+        assert_equal(saplingAddrInfo1["address"], saplingAddr1)
         assert_equal(self.nodes[3].z_getbalance(saplingAddrInfo1["address"]), Decimal('5'))
 
         # Verify that z_gettotalbalance only includes watch-only addresses when requested
